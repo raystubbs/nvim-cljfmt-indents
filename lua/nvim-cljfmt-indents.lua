416f5f49b285b3546b18f4f44814d7c100def04a
Ray Stubbs <stubbs.ray@gmail.com>
2024-09-22 19:10:17 -0600

    initial

---
+++ lua/nvim-cljfmt-indents.lua
@@ -0,0 +1,346 @@
+local edn = require 'edn'
+local plugin = {}
+local vim = _G['vim']
+local max_config_search_level = 15
+
+local function decode_edn (x)
+  return edn.decode(x, {
+    tags = {re = function (s) return vim.regex(s) end},
+    keyword = function (s) return ':' .. s end
+  })
+end
+
+local function read_edn_file (path)
+  local f = io.open(path)
+  if f == nil then
+    return false, nil
+  end
+
+  local success, value = pcall(
+    function ()
+      return decode_edn(f)
+    end
+  )
+  io.close(f)
+  return success, value
+end
+
+local function resolve_lsp_config_edn (dir_path, level)
+  local path = dir_path .. '.lsp/config.edn'
+  local success, value = read_edn_file(path)
+  if success then
+    return value
+  end
+
+  if value ~= nil then
+    print("Error reading the clojure-lsp config file at '" .. path .. "': " .. value)
+  end
+
+  level = level or 0
+  if level > max_config_search_level then
+    return nil
+  else
+    return resolve_lsp_config_edn(dir_path .. '../', level + 1)
+  end
+end
+
+local function resolve_cljfmt_config_edn (dir_path, level)
+  for _, path in ipairs({dir_path .. 'cljfmt.edn', dir_path .. '.cljfmt.edn'}) do
+    local success, value = read_edn_file(path)
+    if success then
+      return value
+    end
+
+    if value ~= nil then
+      print("Error reading the cljfmt config file at '" .. path .. "': " .. value)
+    end
+  end
+
+  level = level or 0
+  if level > max_config_search_level then
+    return nil
+  else
+    return resolve_cljfmt_config_edn(dir_path .. '../', level + 1)
+  end
+end
+
+local function resolve_cljfmt_config (dir_path)
+  local lsp_config = resolve_lsp_config_edn(dir_path)
+  if lsp_config ~= nil and type(lsp_config['cljfmt']) == 'table' then
+    return lsp_config['cljfmt']
+  end
+
+  if lsp_config ~= nil and type(lsp_config['cljfmt-config-path']) == 'string' then
+    local path = lsp_config['cljfmt-config-path']
+    local success, value = read_edn_file(path)
+    if success then
+      return value
+    else
+      print("Error reading the cljfmt config file at '" .. path .. "'")
+    end
+  end
+
+  return resolve_cljfmt_config_edn(dir_path)
+end
+
+local default_indent_edn_strings = {
+  ["clojure.clj"] = [[
+  {alt!            [[:block 0] ]
+   alt!!           [[:block 0] ]
+   are             [[:block 2] ]
+   as->            [[:block 2] ]
+   binding         [[:block 1] ]
+   bound-fn        [[:inner 0] ]
+   case            [[:block 1] ]
+   catch           [[:block 2] ]
+   comment         [[:block 0] ]
+   cond            [[:block 0] ]
+   condp           [[:block 2] ]
+   cond->          [[:block 1] ]
+   cond->>         [[:block 1] ]
+   def             [[:inner 0] ]
+   defmacro        [[:inner 0] ]
+   defmethod       [[:inner 0] ]
+   defmulti        [[:inner 0] ]
+   defn            [[:inner 0] ]
+   defn-           [[:inner 0] ]
+   defonce         [[:inner 0] ]
+   defprotocol     [[:block 1] [:inner 1] ]
+   defrecord       [[:block 2] [:inner 1] ]
+   defstruct       [[:block 1] ]
+   deftest         [[:inner 0] ]
+   deftype         [[:block 2] [:inner 1] ]
+   delay           [[:block 0] ]
+   do              [[:block 0] ]
+   doseq           [[:block 1] ]
+   dotimes         [[:block 1] ]
+   doto            [[:block 1] ]
+   extend          [[:block 1] ]
+   extend-protocol [[:block 1] [:inner 1] ]
+   extend-type     [[:block 1] [:inner 1] ]
+   fdef            [[:inner 0] ]
+   finally         [[:block 0] ]
+   fn              [[:inner 0] ]
+   for             [[:block 1] ]
+   future          [[:block 0] ]
+   go              [[:block 0] ]
+   go-loop         [[:block 1] ]
+   if              [[:block 1] ]
+   if-let          [[:block 1] ]
+   if-not          [[:block 1] ]
+   if-some         [[:block 1] ]
+   let             [[:block 1] ]
+   letfn           [[:block 1] [:inner 2 0] ]
+   locking         [[:block 1] ]
+   loop            [[:block 1] ]
+   match           [[:block 1] ]
+   ns              [[:block 1] ]
+   proxy           [[:block 2] [:inner 1] ]
+   reify           [[:inner 0] [:inner 1] ]
+   struct-map      [[:block 1] ]
+   testing         [[:block 1] ]
+   thread          [[:block 0] ]
+   try             [[:block 0] ]
+   use-fixtures    [[:inner 0] ]
+   when            [[:block 1] ]
+   when-first      [[:block 1] ]
+   when-let        [[:block 1] ]
+   when-not        [[:block 1] ]
+   when-some       [[:block 1] ]
+   while           [[:block 1] ]
+   with-local-vars [[:block 1] ]
+   with-open       [[:block 1] ]
+   with-out-str    [[:block 0] ]
+   with-precision  [[:block 1] ]
+   with-redefs     [[:block 1] ]}
+  ]],
+  ["compojure.clj"] = [[
+  {ANY        [[:inner 0] ]
+   DELETE     [[:inner 0] ]
+   GET        [[:inner 0] ]
+   HEAD       [[:inner 0] ]
+   OPTIONS    [[:inner 0] ]
+   PATCH      [[:inner 0] ]
+   POST       [[:inner 0] ]
+   PUT        [[:inner 0] ]
+   context    [[:inner 0] ]
+   defroutes  [[:inner 0] ]
+   let-routes [[:block 1] ]
+   rfn        [[:inner 0] ]}
+  ]],
+  ["fuzzy.clj"] = [[
+  {#re "^def"    [[:inner 0] ]
+   default       [[:default] ]
+   deflate       [[:default] ]
+   defer         [[:default] ]
+   #re "^with-"  [[:inner 0] ]}
+  ]]
+}
+
+local function get_rule (buf, ts_parent_node, index, depth)
+  if ts_parent_node == nil then
+    return 'default'
+  end
+
+  local is_list = ts_parent_node:type() == 'list_lit'
+  local first_child = is_list and ts_parent_node:named_child(0)
+  if first_child then
+    local first_child_start_row, first_child_start_col = first_child:start()
+    local first_child_end_row, first_child_end_col = first_child:end_()
+    local first_child_str = table.concat(vim.api.nvim_buf_get_text(
+      buf, first_child_start_row, first_child_start_col,
+      first_child_end_row, first_child_end_col, {}
+    ))
+    for _, indent in ipairs(plugin.config.indents) do
+      if indent.matcher(first_child_str) then
+        for _, pattern_rule in ipairs(indent.rules) do
+          if pattern_rule[1] == ":default" and depth == 0 then
+            return 'default'
+          elseif pattern_rule[1] == ":inner" then
+            local rule_depth = pattern_rule[2]
+            local rule_index = pattern_rule[3]
+            if (type(rule_depth) ~= 'number' or rule_depth == depth)
+              and (type(rule_index) ~= 'number' or rule_index == index - 1) then
+              return 'inner'
+            end
+          elseif pattern_rule[1] == ':block'
+            and type(pattern_rule[2]) == 'number'
+            and depth == 0 then
+            local rule_index = pattern_rule[2]
+            local child_at_index = ts_parent_node:named_child(rule_index + 1)
+            if child_at_index ~= nil then
+              local row, col = child_at_index:start()
+              local prefix = table.concat(
+                vim.api.nvim_buf_get_text(buf, row, 0, row, col, {})
+              )
+              if index > rule_index and string.match(prefix, "^%s*$") then
+                return 'inner'
+              else
+                return 'default'
+              end
+            end
+            return 'inner'
+          end
+        end
+      end
+    end
+  end
+
+  local ts_parent_node_index = 0
+  local cursor = ts_parent_node:prev_named_sibling()
+  while cursor ~= nil do
+    ts_parent_node_index = ts_parent_node_index + 1
+    cursor = cursor:prev_named_sibling()
+  end
+  return get_rule(buf, ts_parent_node:parent(), ts_parent_node_index, depth + 1)
+end
+
+local function get_indentation(buf, pos)
+  pos = pos or vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())
+  buf = buf or vim.api.nvim_get_current_buf()
+
+  local parser = plugin.parser or vim.treesitter.get_parser(buf, 'clojure')
+  plugin.parser = parser
+
+  if parser == nil then
+    return nil
+  end
+
+  parser:parse({0, pos[1]})
+  local node = vim.treesitter.get_node({bufnr = buf, pos = {pos[1] - 1, pos[2]}})
+
+  while node
+    and node:type() ~= 'list_lit'
+    and node:type() ~= 'map_lit'
+    and node:type() ~= 'vec_list'
+    and node:type() ~= 'set_lit'
+    and node:type() ~= 'str_lit' do
+      node = node:parent()
+    end
+
+  if node == nil then
+    return nil
+  end
+
+  local node_row, node_col = node:start()
+
+  if node:type() == 'str_lit' then
+    return nil
+  elseif node:type() ~= 'list_lit' then
+    return node_col + 1
+  end
+
+  local index = 0
+  while node:named_child(index) and node:named_child(index):end_() < pos[1] - 1 do
+    index = index + 1
+  end
+
+  local rule = get_rule(buf, node, index, 0)
+
+  if rule == 'default' then
+    local second_child = node:named_child(1)
+    if second_child then
+      local row, col = second_child:start()
+      if row == node_row then
+        return col
+      end
+    end
+
+    return node_col + 1
+  elseif rule == 'inner' then
+    return node_col + 2
+  end
+
+  return nil
+end
+
+function plugin.setup (opts)
+  opts = opts or {}
+  plugin.config = {}
+
+  if type(opts.config) == 'table' then
+    plugin.config.cljfmt = opts.config
+  elseif type(opts.config) == 'string' then
+    local success, value = read_edn_file(opts.config)
+    if success then
+      plugin.config.cljfmt = value
+    else
+      print("Error reading the cljfmt config file at '" .. plugin.config .. "'")
+    end
+    plugin.config.cljfmt = resolve_cljfmt_config('./')
+  else
+    plugin.config.cljfmt = resolve_cljfmt_config('./') or {}
+  end
+
+  local indents = {}
+  local function add_indent(k, v)
+    if type(k) == 'string' then
+      table.insert(indents, {priority = 0, matcher = function(s) return s == k end, rules = v})
+    elseif type(k) == 'userdata' and string.format('%s', k) == '<regex>' then
+      table.insert(indents, {priority = 9, matcher = function(s) return k:match_str(s) end, rules = v})
+    end
+  end
+  if type(plugin.config.cljfmt.indents) == 'table' then
+    for k, v in pairs(plugin.config.cljfmt.indents) do
+      add_indent(k, v)
+    end
+  else
+    for _, indents_edn_string in pairs(default_indent_edn_strings) do
+      for k, v in pairs(decode_edn(indents_edn_string)) do
+        add_indent(k, v)
+      end
+    end
+  end
+  for k, v in pairs(plugin.config.cljfmt['extra-indents'] or {}) do
+    add_indent(k, v)
+  end
+
+  table.sort(indents, function(a, b) return a.priority < b.priority end)
+
+  plugin.config.indents = indents
+  vim.g.GetCljfmtIndent = function()
+    return get_indentation() or -1
+  end
+end
+
+return plugin
